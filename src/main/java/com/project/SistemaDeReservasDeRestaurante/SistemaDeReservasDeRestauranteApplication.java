428b6dd5979120af042e6d394770ffeced078426
Moisés Ferreira <moisesaoaoao@gmail.com>
2025-08-02 23:15:43 -0300

    First Commit

---
+++ src/main/java/com/project/SistemaDeReservasDeRestaurante/SistemaDeReservasDeRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.project.SistemaDeReservasDeRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeReservasDeRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeReservasDeRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/SistemaDeReservasDeRestaurante/SistemaDeReservasDeRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.SistemaDeReservasDeRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeReservasDeRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
